c0b0a6f123578587d10528b7b7231575214faf4a
Gymnae <dagorwaith@gmail.com>
2016-04-10 12:32:20 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -7,7 +7,7 @@ WELCOMETEXT=${WELCOMETEXT:-}
 #insert the password
 sed -i.bak 's/'REPLACEME'/'$SERVERPASSWORD'/g' /etc/mumble/config/mumble-server.ini
 #insert the welcometext
-sed -i.bak 's/'WELCOMETEXT'/'$WELCOMETEXT'/g' /etc/mumble/config/mumble-server.ini
+sed -i.bak "s;WELCOMETEXT;$WELCOMETEXT;g" /etc/mumble/config/mumble-server.ini
 wait
 # start the server
 /opt/mumble/murmur.x86 -fg -ini /etc/mumble/config/mumble-server.ini
