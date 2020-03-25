2ac002b301e0a18d16ca77a1dce25571d8367867
Markus Ressel <mail@markusressel.de>
2020-03-25 02:32:58 +0100

    aligned echo to 'grep' command used for the autogenerated password to provide a helpful message for users

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 if [[ ! "$SUPERUSER_PASSWORD" == "" ]]; then
     /opt/mumble/murmur.x86 -fg -ini $CONFIG_PATH -supw $SUPERUSER_PASSWORD
-    echo "SuperUser password set from environment variable"
+    echo "Password for 'SuperUser' was set from environment variable"
     unset SUPERUSER_PASSWORD
 fi
 
