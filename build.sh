322c2cb193eab6df2ed27792cc807c446d360f36
Gymnae <dagorwaith@gmail.com>
2021-02-07 16:22:54 +0000

    adjusting for bigger welcome message

---
+++ build.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+/usr/bin/docker build --pull --rm -t gymnae/mumbleserver_next .
--- init.sh
+++ init.sh
@@ -1,14 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
 #take the password / welcometext from ENV, if none was delivered, replace with empty string
 SERVERPASSWORD=${SERVERPASSWORD:-}
-WELCOMETEXT=${"WELCOMETEXT":-}
-WELCOMETEXTFILE=${welcometextfile:-}
+WELCOMETEXT=${WELCOMETEXT:-}
+welcome_text=`cat $WELCOMETEXTFIlE`
 
 #insert the password
 sed -i.bak 's/'REPLACEME'/'$SERVERPASSWORD'/g' /etc/mumble/config/mumble-server.ini
 #insert the welcometext
-sed -i.bak 's;WELCOMETEXT;"$WELCOMETEXT";g' /etc/mumble/config/mumble-server.ini
+sed -i.bak 's;'WELCOMETEXT';'$WELCOMETEXT';g' /etc/mumble/config/mumble-server.ini
+#insert welcometextfile
+#sed -i.bak "/welcometext=/a\welcometextfile=$WELCOMETEXTFIlE" /etc/mumble/config/mumble-server.ini
+#sed -i.bak 's;welcometext="";welcometext=\"'$WELCOMETEXTFIlE'\";g' /etc/mumble/config/mumble-server.ini
+sed -i.bak "s|welcometext=\"\"|welcometext=\"$welcome_text\"|" /etc/mumble/config/mumble-server.ini
 wait
 # start the server
 /opt/mumble/murmur.x86 -fg -ini /etc/mumble/config/mumble-server.ini
