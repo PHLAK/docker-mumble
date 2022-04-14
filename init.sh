cacf421ca7c746154e63981924efc147c4cdd8b4
Gunnar <dagorwaith@gmail.com>
2022-04-14 20:24:51 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -6,14 +6,14 @@ WELCOMETEXT=${WELCOMETEXT:-}
 welcome_text=`cat $WELCOMETEXTFIlE`
 
 #insert the password
-sed -i.bak 's/'REPLACEME'/'$SERVERPASSWORD'/g' /etc/mumble/config/mumble-server.ini
+sed -i.bak 's/'REPLACEME'/'$SERVERPASSWORD'/g' /etc/murmur/murmur.ini
 #insert the welcometext
-sed -i.bak 's;'WELCOMETEXT';'$WELCOMETEXT';g' /etc/mumble/config/mumble-server.ini
+sed -i.bak 's;'WELCOMETEXT';'$WELCOMETEXT';g' /etc/murmur/murmur.ini
 #insert welcometextfile
-#sed -i.bak "/welcometext=/a\welcometextfile=$WELCOMETEXTFIlE" /etc/mumble/config/mumble-server.ini
-#sed -i.bak 's;welcometext="";welcometext=\"'$WELCOMETEXTFIlE'\";g' /etc/mumble/config/mumble-server.ini
-#sed -i.bak "s|welcometext=\"\"|welcometext=\"$welcome_text\"|" /etc/mumble/config/mumble-server.ini
-sed -i -e '/welcometext=""/{r '$WELCOMETEXTFIlE'' -e 'd}' /etc/mumble/config/mumble-server.ini
+#sed -i.bak "/welcometext=/a\welcometextfile=$WELCOMETEXTFIlE" /etc/murmur/murmur.ini
+#sed -i.bak 's;welcometext="";welcometext=\"'$WELCOMETEXTFIlE'\";g' /etc/murmur/murmur.ini
+#sed -i.bak "s|welcometext=\"\"|welcometext=\"$welcome_text\"|" /etc/murmur/murmur.ini
+sed -i -e '/welcometext=""/{r '$WELCOMETEXTFIlE'' -e 'd}' /etc/murmur/murmur.ini
 wait
 # start the server
-/opt/mumble/murmur.x86 -fg -ini /etc/mumble/config/mumble-server.ini
+/usr/bin/mumble-server -v -fg -ini /etc/murmur/murmur.ini
